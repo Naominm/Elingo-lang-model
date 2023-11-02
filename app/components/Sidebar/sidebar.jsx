467104a21f32222071b5611e429502b34297ac71
DavidKMuchiri <dkamau716@gmail.com>
2023-11-02 21:44:17 +0300

    Improvements

--- app/components/Sidebar/sidebar.jsx
+++ app/components/Sidebar/sidebar.jsx
@@ -5,9 +5,9 @@ import { TbSettings2 } from 'react-icons/tb';
 
 function Sidebar() {
   return (
-    <div className="absolute h-[87%]  overflow-y-auto w-1/4 bg-[#1389A3] text-white flex flex-col items-start mt-0 rounded-r-lg sidebar">
-      <div className="relative mt-10 ml-10 w-40 flex flex-col flex-grow">
-        <div className="relative mt-10 ml-0 flex flex-col">
+    <div className="absolute h-full w-1/4 bg-[#1389A3] text-white flex flex-col items-start mt-0 rounded-r-lg sidebar">
+      <div className="relative mt-5 ml-10 w-40 flex flex-col flex-grow">
+        <div className="relative ml-0 flex flex-col">
           <div className="flex items-center p-5">
             <div className="flex items-center font-bold text-white">
               <div className="mr-2">
@@ -19,7 +19,7 @@ function Sidebar() {
           <div className="flex items-center p-5">
             <div className="flex items-center font-bold text-white">
               <div className="mr-2">
-                <FcAbout className='text-2xl' />
+                <FcAbout className='text-2xl color-white' />
               </div>
               About
             </div>
